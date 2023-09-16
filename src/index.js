b35eeeed138e554cd91aa08f62a4399b40aca193
Kelvin Cheng <kerwin145@gmail.com>
2023-09-15 23:01:49 -0400

    Added search bar functionality

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import Model from './model.js';
 
 //TODO Add question tag click filter functionality
 
+const main = document.querySelector('#main')
+const content = document.querySelector('.content')
+
 const questionList = document.querySelector("#questionList")
 const questionCount = document.querySelector("#questionNumber")
+const questionsEmptyAlert = document.querySelector("#questionList-emptyAlert")
 
 const sortNewest = document.querySelector('#sortNewest')
 const sortActive = document.querySelector('#sortActive')
 const sortUnanswered = document.querySelector('#sortUnanswered')
+const askQuestion = document.querySelector('.content-askquestion')
+
+const searchbarForm = document.querySelector('#searchbar-form') //we need this for the onsubmit listener to work correctly
+const searchbar = document.querySelector('.searchbar')
+
+let searchStr = "" 
 
 const SortEnum = {
   NEWEST: "newest",
@@ -16,7 +26,6 @@ const SortEnum = {
 };
 
 let selectedSort = SortEnum.NEWEST;
-
 const filterTags = document.querySelector('#tagFilters')
 let tagFilters = [] //filter questions based on tags
 
@@ -30,6 +39,7 @@ function sortQuestionsByNew(){
   selectedSort = SortEnum.NEWEST
   questions =  data.questions.sort((a,b) => b.askDate.getTime() - a.askDate.getTime())
   filterQuestions()
+  searchQuestions()
   loadHomeQuestions()
 }
 
@@ -48,6 +58,7 @@ function sortQuestionsByActive(){
       ))
   )  
   filterQuestions()
+  searchQuestions()
   loadHomeQuestions()
 }
 
@@ -55,6 +66,7 @@ function sortQuestionsByUnanswered(){
   selectedSort = SortEnum.UNANSWERED
   questions = data.questions.filter((x) => x.ansIds.length == 0)
   filterQuestions()
+  searchQuestions()
   loadHomeQuestions()
 }
 
@@ -67,7 +79,7 @@ function deleteTagFilter(tagName, tagId){
   if(tagFilters.length == 0)
     filterTags.innerHTML = ""
 
-    filterAndSortQuestions()
+    sortFilterSearchQuestions()
     loadHomeQuestions()
 }
 function addTagFilter(tagName, tagId){
@@ -97,19 +109,26 @@ function addTagFilter(tagName, tagId){
   
     filterTags.appendChild(tagContainer);
   
-    filterQuestions()
+    sortFilterSearchQuestions()
     loadHomeQuestions()
 }
 
 window.onload = function() {
-  filterAndSortQuestions()
+  sortFilterSearchQuestions()
   loadHomeQuestions()
   
   sortNewest.addEventListener('click', sortQuestionsByNew)
   sortActive.addEventListener('click', sortQuestionsByActive)
   sortUnanswered.addEventListener('click', sortQuestionsByUnanswered)
-};
-  
+  searchbarForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    searchStr = searchbar.value
+    sortFilterSearchQuestions()
+    loadHomeQuestions()
+  })
+  askQuestion.addEventListener('click', askQuestionPrompt)
+}
+
 function loadHomeQuestions(){
   //CLEAR OUT CHILDREN FROM QUESTIONLIST
   questionList.innerHTML = ""
@@ -120,6 +139,12 @@ function loadHomeQuestions(){
   //needed for timestamp
   const timeNow = new Date()
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  
+  if (questions.length == 0)
+    questionsEmptyAlert.style.display = 'block'
+  else 
+    questionsEmptyAlert.style.display = 'none'
+
   questions.forEach((question) => {
     let questionContainer = document.createElement('div')
     questionContainer.className = "question-container"
@@ -231,9 +256,10 @@ function loadHomeQuestions(){
 }
 
 
-function filterAndSortQuestions(){
+function sortFilterSearchQuestions(){
   sortQuestions()
   filterQuestions()
+  searchQuestions()
 }
 
 function sortQuestions(){
@@ -255,4 +281,87 @@ function filterQuestions(){
     }
     return true
   }) 
+}
+
+function searchQuestions(){
+  //tokens in brackets are tag searches. everythign else is title substring search
+  //If any token matches (be it tag or substring) then the question is returned
+  
+  let searchstr = searchStr //copy searchStr
+  if (searchstr.length < 1)
+    return
+
+  let searchTags = []
+  let searchTerms = []
+  //input handling
+  let tagStart = -2, tagEnd = -1
+  for(let i = 0; i < searchstr.length; i++){
+    let c = searchstr[i]
+
+    //this means that we haven't yet started the window for parsing the tag and should begin
+    if(c == '[' && tagEnd > tagStart){
+      tagStart = i
+    }
+    //this means we ahve started the window, and should close the tag
+    else if( c== ']' && tagEnd < tagStart){
+      tagEnd = i
+      if(tagEnd - tagStart <= 1) //empty string check
+        continue;
+      
+      searchTags.push(searchstr.substring(tagStart+1, tagEnd))
+
+      //modify our searchstr to get rid of the tag and replace it with a space adjust i accordingly. 
+      searchstr = searchstr.substring(0, tagStart) + " " +  searchstr.substring(tagEnd+1, searchstr.length) 
+      i -= (tagEnd-tagStart)
+      tagEnd = -1
+      tagStart = -2
+    }
+    else if( c == '[' || c == ']'){
+      window.alert("malformed brackets in search bar.")
+      return
+    }
+  }
+
+  //remapping tag to their proper id
+  for(let i = 0; i < searchTags.length; i++){
+    let tag = searchTags[i]
+    let tagId = data.tags.find(x => x.name == tag)
+    if (tagId == undefined){
+      window.alert(`\"${tag}\" is not a valid registered tag, or no entries with that tag exist. Note that tags are case sensitive.`)
+      return
+    }
+    
+    searchTags[i] = tagId.tid
+  }
+  
+  searchTerms = searchstr.split(' ').filter(x => x.length > 0)
+
+  questions = questions.filter(question => 
+    question.tagIds.some(tag => searchTags.includes(tag))
+    || searchTerms.some(token => question.title.includes(token))
+  ) 
+
+}
+
+function askQuestionPrompt(){
+	//changes from current page to new question prompt page
+	content.style.display = 'none'
+	
+	//ask question page
+	let askQuestionContainer = document.createElement('div')
+	askQuestionContainer.className = 'askQuestion-container'
+	askQuestionContainer.style.display = 'flex'
+	
+	let askQuestionTitle = document.createElement('h2')
+	askQuestionTitle.className = 'askQuestion-directions'
+	askQuestionTitle.innerHTML = 'Question Title*'
+	
+	let askQuestionTitleDetails = document.createElement('p')
+	askQuestionTitleDetails.className = 'askQuestion-details'
+	askQuestionTitleDetails.innerHTML =  'Limit title to 100 characters or less'
+	
+	askQuestionContainer.appendChild(askQuestionTitle)
+	askQuestionContainer.appendChild(askQuestionTitleDetails)
+	
+	main.appendChild(askQuestionContainer)
 }
